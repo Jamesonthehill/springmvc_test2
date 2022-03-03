d9aaf051c30cc3fa106b40f4cc2ab7ac3c935caa
건우 <dlrjsdn53@naver.com>
2022-03-03 14:47:19 +0900

    CodeDao.java

--- src/main/java/com/junefw/infra/modules/code/CodeDao.java
+++ src/main/java/com/junefw/infra/modules/code/CodeDao.java
@@ -9,7 +9,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CodeDao {
+public class CodeDao { // CodeMpp로 넘겨서 데이터를 받아오ㅓ는 역할을 해준다.
 	
 	@Inject
 	@Resource(name = "sqlSession")
@@ -18,5 +18,8 @@ public class CodeDao {
 	private static String namespace = "com.junefw.infra.modules.code.CodeMpp";
 	
 	public List<Code> selecList() { return sqlSession.selectList(namespace + ".selectList", "");} // List는 배열임 배열에다가 객체를 쭉 받는다 의미
-		
+					//얘 잘보면select 아니고 selec
+	public int insert(Code dto) {return sqlSession.insert(namespace + ".insert", dto);}
+	
+	public Code selectOne(CodeVo vo) { return sqlSession.selectOne(namespace + ".selectOne", vo);}
 }
